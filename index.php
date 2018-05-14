71db590f2bddddb37cd0fc749d5cc47915da26de
sonjooho <35133411+sonjooho@users.noreply.github.com>
2018-05-14 15:31:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-
-
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.socialstudio.radian6.com/v3/topics?status=4",
+  CURLOPT_URL => "https://api.socialstudio.radian6.com/oauth/token",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "grant_type=password&client_id=mcxxe11808eec6214492ba92dc49d8824c3d&client_secret=5ed9314d51f340888ffd36f4364ffbae&username=r6-json&password=Thswnghdudwn%24017",
   CURLOPT_HTTPHEADER => array(
     "Cache-Control: no-cache",
-    "Postman-Token: 6b9c8e8c-91d6-4aca-a0a9-4074d99714b3",
-    "access_token: ac705472-8a2a-4a89-a64c-8166a80b29d5"
+    "Content-Type: application/x-www-form-urlencoded",
+    "Postman-Token: 0d67cb2c-66ce-4198-8b4a-aaf0c77d73a1"
   ),
 ));
 
@@ -23,10 +22,13 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
+$ar = json_decode($response);
+
+echo $ar[0];
+
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
   echo $response;
 }
-
 ?>
