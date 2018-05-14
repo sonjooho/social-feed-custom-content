7a6aba39100f508b413bd0073e2a911e6ce4c25a
sonjooho <35133411+sonjooho@users.noreply.github.com>
2018-05-14 15:04:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://api.socialstudio.radian6.com/oauth/token');
-$request->setMethod(HTTP_METH_POST);
+$curl = curl_init();
 
-$request->setHeaders(array(
-  'Content-Type' => 'application/x-www-form-urlencoded',
-  'Postman-Token' => '99bb970a-3936-49f3-ab98-b226c61787c1',
-  'Cache-Control' => 'no-cache'
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.socialstudio.radian6.com/oauth/token",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "grant_type=password&client_id=mcxxe11808eec6214492ba92dc49d8824c3d&client_secret=5ed9314d51f340888ffd36f4364ffbae&username=r6-json&password=Thswnghdudwn%24017",
+  CURLOPT_HTTPHEADER => array(
+    "Cache-Control: no-cache",
+    "Content-Type: application/x-www-form-urlencoded",
+    "Postman-Token: 0d67cb2c-66ce-4198-8b4a-aaf0c77d73a1"
+  ),
 ));
 
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
-  'grant_type' => 'password',
-  'client_id' => 'mcxxe11808eec6214492ba92dc49d8824c3d',
-  'client_secret' => '5ed9314d51f340888ffd36f4364ffbae',
-  'username' => 'r6-json',
-  'password' => 'Thswnghdudwn$017'
-));
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-  $response = $request->send();
+curl_close($curl);
 
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
+
 ?>
